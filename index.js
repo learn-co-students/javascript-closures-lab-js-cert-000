f66e177bb3276596ec6df7b1f0f0fb34f35d65b2
P Joshi <joshipv@yahoo.com>
2016-08-26 00:06:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+//function closures(counter){
+  //function threePointTurn() {
+    function bumpCounter(counter) {
+        function addBump() {
+          //var cnt = 0;
+          return counter + 1;
+       }
+        function getBumps() {
+          var cnt = 0;
+         return cnt = counter;
+      }
+      return {addBump, getBumps}
+    }
+
+  function createAnimal(animalType) {
+    return function(deadlyDevice) {
+      return { animalType: animalType, deadlyDevice: deadlyDevice }
+    }
+
+  }
+  var sharkCreator = createAnimal('Shark');
+  var sharkWithFrickinLaserbeam = createAnimal({'Shark': 'Laserbeam'});
+  var sharkWithFrickinCannon = createAnimal({'Shark': 'Cannon'});
