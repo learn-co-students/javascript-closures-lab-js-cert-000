ba74629e1a7c7e1bca04a38bbcfd0f8a7f645ea8
Kyle Lee <hello.kylelee@gmail.com>
2017-02-11 03:39:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+
+  function diabolicalCreation(deadlyDevice) {
+    return {
+      animalType,
+      deadlyDevice
+    };
+  }
+
+  return diabolicalCreation;
+
+}
+
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+
+var sharkWithFrickinCannon = sharkCreator('Cannon');
